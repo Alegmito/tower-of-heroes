92e653ffbd098525677540bb87bc098a4f90f53b
Smirnov Daniil <Alegmito>
2022-06-08 16:42:50 +0300

    feat: add hero component

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    {id: 2, name: 'Dildos'},
+    {id: 3, name: 'Nol\''},
+    {id: 4, name: 'Celkovii'},
+    {id: 5, name: 'Checkushka'},
+    {id: 6, name: 'Checkushka'},
+    {id: 7, name: 'Zhuckok'},
+    {id: 8, name: 'Mudachok'},
+    {id: 9, name: 'Sir Hui Na Vorotnichok'},
+    {id: 10, name: 'ShUE'},
+]
