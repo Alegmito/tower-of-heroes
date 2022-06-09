68fe3552b2c71918f188e7ebdaf863b5a0ece11b
Smirnov Daniil <Alegmito>
2022-06-09 09:27:18 +0300

    feat: add hero detail and service components

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,11 +1,12 @@
 import { Hero } from "./hero";
 
 export const HEROES: Hero[] = [
+    {id: 1, name: 'Windstorm'},
     {id: 2, name: 'Dildos'},
     {id: 3, name: 'Nol\''},
     {id: 4, name: 'Celkovii'},
     {id: 5, name: 'Checkushka'},
-    {id: 6, name: 'Checkushka'},
+    {id: 6, name: 'Kolotushka'},
     {id: 7, name: 'Zhuckok'},
     {id: 8, name: 'Mudachok'},
     {id: 9, name: 'Sir Hui Na Vorotnichok'},
